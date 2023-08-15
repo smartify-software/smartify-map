a6311ac6af9857bdd323c781542d2a72620c9fca
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2023-08-15 11:04:06 -0400

    trying out my luck with vue3-google-map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,53 +1,19 @@
 <template>
-  <div class="map-container">
-    <GMapMap
-        :center="center"
-        :zoom="15"
-        map-type-id="terrain"
-        class="map"
-    >
-      <GMapMarker
-          :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :clickable="true"
-          :draggable="true"
-          @click="center=m.position"
-      />
-    </GMapMap>
-  </div>
+  <GoogleMap api-key="AIzaSyBkA4jYjWyzhbedSSeCEnG0iDwb6o5QMtI" style="width: 100%; height: 500px" :center="center" :zoom="15">
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      center: {lat: 38.8895, lng: -77.0352},
-      markers: [
-        {
-          position: {
-            lat: 51.093048, lng: 6.842120
-          },
-        }
-        // Along list of clusters
-      ]
-    }
-  }
-}
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 };
+
+    return { center };
+  },
+});
 </script>
-
-<style scoped>
-.map-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-}
-
-.map {
-  width: 80%;
-  height: 80%;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import {createApp} from 'vue'
-import VueGoogleMaps from '@fawmi/vue-google-maps'
 import App from './App.vue'
 
 const app = createApp(App);
-app.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyBkA4jYjWyzhbedSSeCEnG0iDwb6o5QMtI',
-    },
-}).mount('#app')
+app.mount('#app')
 
 

e94901fec4988a79a81048e92381e56b411c762b
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2023-08-15 10:16:57 -0400

    Basic map is working

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,35 +1,53 @@
 <template>
-  <GMapMap
-      :center="center"
-      :zoom="7"
-      map-type-id="terrain"
-      style="width: 500px; height: 500px"
-  >
-    <GMapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        :clickable="true"
-        :draggable="true"
-        @click="center=m.position"
-    />
-  </GMapMap>
+  <div class="map-container">
+    <GMapMap
+        :center="center"
+        :zoom="15"
+        map-type-id="terrain"
+        class="map"
+    >
+      <GMapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+      />
+    </GMapMap>
+  </div>
 </template>
+
 <script>
 export default {
   name: 'App',
   data() {
     return {
-      center: {lat: 51.093048, lng: 6.842120},
+      center: {lat: 38.8895, lng: -77.0352},
       markers: [
         {
           position: {
             lat: 51.093048, lng: 6.842120
           },
         }
-        , // Along list of clusters
+        // Along list of clusters
       ]
     }
   }
 }
 </script>
+
+<style scoped>
+.map-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.map {
+  width: 80%;
+  height: 100%;
+}
+</style>
